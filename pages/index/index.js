e1498dfdbfadc248bf5d6edb7032334cafbfd1c2
heboot <heboot@126.com>
2017-06-02 21:00:19 +0800

    init project
    init project

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    imgUrls: [
+      "../images/ban1.jpg",
+      "../images/ban3.jpg",
+    ],
+    comments: [
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+      { name: "上海花木科技信息有限公司  曾经理", content: "做的软件很好用，也符合预期，简直炸裂！。", date: "2017年2月4日" },
+
+
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+  }
+})
